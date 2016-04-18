61dce302bc3dd0c89bd13375f1b65c67567692ae
Robert Koeppl <rkoeppl@gmx.at>
2016-04-19 00:01:21 +0200

    added ned folder for functional modules created new file for perl based print engine and configuration for that engine

---
+++ modules/printengine.pl
@@ -0,0 +1,9 @@
+#!/usr/bin/perl
+#print engine to be called by SLAcer.js and other printer services
+#Copyright 2016 Robert Koeppl, Fablab Leoben robert.koeppl@fablab-leoben.at
+#http://www.fablab-leoben.at
+#http://www.github.com/fablab-leoben
+#released under the MIT License
+#this piece of software is provided with absolutely no warranty
+#use at your own risk
+#configuration is stored in printengine.cfg, do not use hardcoded configuration in ths per script, that is bad practice.
