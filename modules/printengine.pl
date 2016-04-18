4d30ff776ceb2cdc5ef73f95b158a03021c096a5
Robert Koeppl <rkoeppl@gmx.at>
2016-04-19 01:33:13 +0200

    configured and defined basic variables in configuration-module started to introduce primitive samity-checks and configuration evaluation to printengime untill further notice onyl Beaglebone black with Lightcrafter 4500 on HDMI will be supported.

--- modules/printengine.pl
+++ modules/printengine.pl
@@ -1,5 +1,5 @@
 #!/usr/bin/perl
-#print engine to be called by SLAcer.js and other printer services
+#print engine to be called by SLAcer.js and other printer services orstandalone from command line
 #Copyright 2016 Robert Koeppl, Fablab Leoben robert.koeppl@fablab-leoben.at
 #http://www.fablab-leoben.at
 #http://www.github.com/fablab-leoben
@@ -7,3 +7,27 @@
 #this piece of software is provided with absolutely no warranty
 #use at your own risk
 #configuration is stored in printengine.cfg, do not use hardcoded configuration in ths per script, that is bad practice.
+use warning;
+use strict;
+use Getopt::Std;
+use Getopt::Long;
+use feature qw(say);
+
+#include configuration and settings from printengine.cfg
+use printengine.cfg
+#checked for used controller board type according to configuration.
+#activate logging to logfile
+if $logging_enabled eq "TRUE" then
+{
+open my $log_fh, ">", $log_file;
+}
+
+if $controllerboard eq "BBB" then 
+{
+
+}
+else {
+say "unknows printer type $controllerboard , please review your configuration, get in touch with developers or fork the code on Github and contribute the code to use the new printer"
+;
+die "unknown board in configuration!\n";
+}
